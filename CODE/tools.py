43a7a722677465b6717565e3791c5e7f09a2530d
fpesceKU <57720021+fpesceKU@users.noreply.github.com>
2024-04-04 18:11:47 +0200

    Update tools.py

--- CODE/tools.py
+++ CODE/tools.py
@@ -1,5 +1,9 @@
 import pandas as pd
 from localcider.sequenceParameters import SequenceParameters
+import sys
+import numpy as np
+import MDAnalysis
+from MDAnalysis import transformations
 
 def fix_topology(t,seq):
     cgtop = md.Topology()
@@ -54,3 +58,62 @@ def rg(t, seq):
     masses = residues.loc[seq,'MW'].values
     rgarray = md.compute_rg(t,masses=masses)
     return rgarray
+
+def calc_zpatch(z,h):
+    cutoff = 0
+    ct = 0.
+    ct_max = 0.
+    zwindow = []
+    hwindow = []
+    zpatch = []
+    hpatch = []
+    for ix, x in enumerate(h):
+        if x > cutoff:
+            ct += x
+            zwindow.append(z[ix])
+            hwindow.append(x)
+        else:
+            if ct > ct_max:
+                ct_max = ct
+                zpatch = zwindow
+                hpatch = hwindow
+            ct = 0.
+            zwindow = []
+            hwindow = []
+    zpatch = np.array(zpatch)
+    hpatch = np.array(hpatch)
+    return zpatch, hpatch
+
+def center_slab(name,start=None,end=None,step=1,input_pdb='top.pdb'):
+    path = f'{name}'
+    u = MDAnalysis.Universe(f'{path}/{input_pdb}',path+f'/{name}.dcd',in_memory=True)
+    n_frames = len(u.trajectory[start:end:step])
+    ag = u.atoms
+    n_atoms = ag.n_atoms
+
+    L = u.dimensions[0]/10
+    lz = u.dimensions[2]
+    edges = np.arange(0,lz+1,1)
+    dz = (edges[1] - edges[0]) / 2.
+    z = edges[:-1] + dz
+    n_bins = len(z)
+    hs = np.zeros((n_frames,n_bins))
+    with MDAnalysis.Writer(path+'/traj.dcd',n_atoms) as W:
+        for t,ts in enumerate(u.trajectory[start:end:step]):
+            # shift max density to center
+            zpos = ag.positions.T[2]
+            h, e = np.histogram(zpos,bins=edges)
+            zmax = z[np.argmax(h)]
+            ag.translate(np.array([0,0,-zmax+0.5*lz]))
+            ts = transformations.wrap(ag)(ts)
+            zpos = ag.positions.T[2]
+            h, e = np.histogram(zpos, bins=edges)
+            zpatch, hpatch = calc_zpatch(z,h)
+            zmid = np.average(zpatch,weights=hpatch)
+            ag.translate(np.array([0,0,-zmid+0.5*lz]))
+            ts = transformations.wrap(ag)(ts)
+            zpos = ag.positions.T[2]
+            h, e = np.histogram(zpos,bins=edges)
+            hs[t] = h
+            W.write(ag)
+    np.save(f'{path}/{name}.npy',hs,allow_pickle=False)
